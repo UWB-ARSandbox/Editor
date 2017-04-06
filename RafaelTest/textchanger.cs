6c1c61b25e370de9e030f48ce674b6bdceebc16b
eunaoquero <eunaoquero@gmail.com>
2017-04-06 16:22:52 -0700

    network shared memory test

---
+++ RafaelTest/textchanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textchanger : MonoBehaviour {
+	private TextMesh textstring;
+
+	void Start () {
+		textstring = transform.GetComponentInChildren<TextMesh> ();
+
+
+		changeText ("YEAH!!!");
+		Debug.Log ("test");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+	public void changeText(string changeTMP){
+
+
+		textstring.text = changeTMP;
+
+	}
+}
