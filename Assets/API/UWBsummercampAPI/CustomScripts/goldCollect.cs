64d305044bf780bc4bbbb2f4dbfe2746c0919e8a
arridoarfiadi <arridoarfiadi@hotmail.com>
2017-08-06 15:51:20 -0700

    VR Demo Edit

--- Assets/API/UWBsummercampAPI/CustomScripts/goldCollect.cs
+++ Assets/API/UWBsummercampAPI/CustomScripts/goldCollect.cs
@@ -1,21 +1,21 @@
 using UnityEngine;
 using UWBsummercampAPI;
 
-public class jump : coreObjectsBehavior {
+public class goldCollect : coreObjectsBehavior {
 	
 	// buildGame() is called once, at the start
 	// of the game
 	void buildGame () {
-		
+		spinObject (true);
 	}
-	
+
 	// updateGame() is called many times per
 	// second
 	void updateGame () {
 		if(playerIsTouching()){
-			jumpPlayer(50);
-
+			givePoints (50);
+			destroyObject();
 		}
+
 	}
-	
 }
